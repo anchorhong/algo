90c776a68b90f4da62c66248ebbc8ebc1047675c
Anchor Hong <anchorhong@outlook.com>
2021-02-20 23:06:52 +0800

    finish the 765

---
+++ 0765/minSwapsCouples.go
@@ -0,0 +1,79 @@
+package _765
+
+func minSwapsCouples(row []int) int {
+	n := len(row)
+	uf := newUnionFind(n / 2)
+	for i := 0; i < n; i += 2 {
+		uf.union(row[i]/2, row[i+1]/2)
+	}
+	return n/2 - uf.count
+}
+
+func minSwapCouplesBFS(row []int) int {
+	n := len(row)
+	edges := make([][]int, n/2)
+	for i := 0; i < n; i += 2 {
+		l, r := row[i]/2, row[i+1]/2
+		if l != r {
+			edges[l] = append(edges[l], r)
+			edges[r] = append(edges[r], r)
+		}
+	}
+	ans := 0
+	visited := make([]bool, n/2)
+	for i, v := range visited {
+		if !v {
+			visited[i] = true
+			cnt := 0
+			q := []int{i}
+			for len(q) != 0 {
+				cnt++
+				q = q[1:]
+				for _, u := range edges[i] {
+					if !visited[u] {
+						q = append(q, u)
+					}
+				}
+			}
+			ans += cnt - 1
+		}
+	}
+	return ans
+}
+
+type unionFind struct {
+	parent, rank []int
+	count        int
+}
+
+func newUnionFind(n int) *unionFind {
+	parent := make([]int, n)
+	rank := make([]int, n)
+	for i := range parent {
+		parent[i] = i
+		rank[i] = 1
+	}
+	return &unionFind{
+		parent, rank, n,
+	}
+}
+
+func (u *unionFind) find(p int) int {
+	if u.parent[p] != p {
+		u.parent[p] = u.find(u.parent[p])
+	}
+	return u.parent[p]
+}
+
+func (u *unionFind) union(p, q int) {
+	pRoot, qRoot := u.find(p), u.find(q)
+	if pRoot == qRoot {
+		return
+	}
+	if u.rank[pRoot] < u.rank[qRoot] {
+		pRoot, qRoot = qRoot, pRoot
+	}
+	u.parent[qRoot] = pRoot
+	u.rank[pRoot] += u.rank[qRoot]
+	u.count--
+}
---
+++ 0765/minSwapsCouples_test.go
@@ -0,0 +1,70 @@
+package _765
+
+import "testing"
+
+func Test_minSwapsCouples(t *testing.T) {
+	type args struct {
+		row []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "testCase 1",
+			args: args{
+				row: []int{0, 2, 1, 3},
+			},
+			want: 1,
+		},
+		{
+			name: "testCase 2",
+			args: args{
+				row: []int{3, 2, 0, 1},
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minSwapsCouples(tt.args.row); got != tt.want {
+				t.Errorf("minSwapsCouples() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_minSwapCouplesBFS(t *testing.T) {
+	type args struct {
+		row []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+
+		{
+			name: "testCase 1",
+			args: args{
+				row: []int{0, 2, 1, 3},
+			},
+			want: 1,
+		},
+		{
+			name: "testCase 2",
+			args: args{
+				row: []int{3, 2, 0, 1},
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minSwapCouplesBFS(tt.args.row); got != tt.want {
+				t.Errorf("minSwapCouplesBFS() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
